80c10a4ff63ea36cd55aa14a8a01d49c46f24321
innazl <mozyr.innazl@tut.by>
2018-09-30 01:49:07 +0300

    money-exchange: task is done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    mincoins = {};
+    if (currency > 10000) {
+        mincoins['error'] = "You are rich, my friend! We don't have so much coins for exchange";
+    } else if (currency > 0) {
+        find('H', 50);
+        find('Q', 25);
+        find('D', 10);
+        find('N', 5);
+        find('P', 1);
+    };
+    return mincoins; 
+    function find(nominal, value) {
+        let tempvalue = Math.floor(currency / value); 
+        if (tempvalue > 0) {
+            mincoins[nominal] = tempvalue;
+            currency = currency % value;
+        }
+    }    
 }
